c9b92cabcc3770ca8d7ba75966e08f7c8a67b50d
Burdykin Oleg Vladimirovich <Oleg.Burdykin@vaz.ru>
2019-04-30 14:41:55 +0400

    Стартовый проект

---
+++ GenerateTexture/Core.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GenerateTexture
+{
+    public static class ConvertTexture
+    {
+        public static BitmapSource GifToLongBmp(string LoadFilePath)
+        {
+            //
+            GifBitmapDecoder gifDecoder = new GifBitmapDecoder(new Uri(LoadFilePath, UriKind.Relative), BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            //MessageBox.Show(string.Format("{0}", gifDecoder.Frames.Count));
+            //
+            //берем размер кадра
+            BitmapFrame frame0 = gifDecoder.Frames[0];
+            int imageWidth = frame0.PixelWidth;
+            int imageHeight = frame0.PixelHeight;
+            int frameCount = gifDecoder.Frames.Count;
+            // компонент для отрисовки всех кадров
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                for (int i = 0; i < gifDecoder.Frames.Count; i++)
+                {
+                    BitmapFrame frame1 = gifDecoder.Frames[i];
+                    drawingContext.DrawImage(frame1, new Rect(i * imageWidth, 0, imageWidth, imageHeight));
+                }
+            }
+            //конверт в BitmapSource
+            RenderTargetBitmap bmp = new RenderTargetBitmap(imageWidth * frameCount, imageHeight, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+            return bmp;
+        }
+
+        public static void SaveToPng(BitmapSource Bmp, string SaveFilePath)
+        {
+            using (var fileStream = new FileStream(SaveFilePath, FileMode.Create))
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(Bmp));
+                encoder.Save(fileStream);
+            }
+        }
+    }
+}
---
+++ GenerateTexture/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenerateTexture
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //
+            string LoadFilePath = @"Sprites/catloader.gif";
+            string SaveFilePath = @"Sprites/catloader.png";
+            //получение итогового изображения
+            BitmapSource Bmp = ConvertTexture.GifToLongBmp(LoadFilePath);
+            //сжатие итогового изображения в png
+            ConvertTexture.SaveToPng(Bmp, SaveFilePath);
+        }
+
+    }
+}
